d3f5428d3f8a35248204dea45999ce04a63bf941
loadeng01 <120264033+loadeng01@users.noreply.github.com>
2023-11-17 18:49:33 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['16.171.115.199', 'localhost']
+ALLOWED_HOSTS = ['51.20.96.150', 'localhost']
 
 # Application definition
 
