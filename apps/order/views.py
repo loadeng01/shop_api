309213a125ddaabf39fb30cf9306241d56c8b79c
Myrza <loadenggg@gmail.com>
2023-11-22 10:39:25 +0600

    add order views

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+
+class OrderApiView(ListCreateAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = IsAuthenticated,
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        orders = user.orders.all()
+        serializer = self.serializer_class(instance=orders, many=True)
+        return Response(serializer.data, status=200)
+
+
+class OrderConfirmView(APIView):
+    def get(self, request, pk):
+
+
+
+
+
