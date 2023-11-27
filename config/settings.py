5729daf4b995fb33f1075226be9c7d046184340a
Myrza <loadenggg@gmail.com>
2023-11-27 11:30:17 +0600

    change ip

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['51.20.96.150', 'localhost']
+ALLOWED_HOSTS = ['13.51.169.73', 'localhost']
 
 # Application definition
 
